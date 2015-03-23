0ca495875a97b681b1e36b53cdf6383de8802aaf
jakedye7 <jacob-dye@uiowa.edu>
2015-03-23 12:48:39 -0500

    added sisc and sisc_tb files

---
+++ sisc.v
@@ -0,0 +1,8 @@
+// 55:035 sisc processor project
+// sisc module
+
+`timescale 1ns/100ps
+
+module sisc (CLK, RST_F, ir);
+
+endmodule
---
+++ sisc_tb.v
@@ -0,0 +1,8 @@
+// 55:035 sisc processor project
+// test bench for sisc processor
+
+`timescale 1ns/100ps
+
+module sisc_tb;
+
+endmodule
