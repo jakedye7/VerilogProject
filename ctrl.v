ff7a53e70b117a250b88d084d585bc5f9f9c51b6
jakedye7 <jacob-dye@uiowa.edu>
2015-03-25 17:58:07 -0500

    almost done with ctrl.v, good start on sisc.v

--- ctrl.v
+++ ctrl.v
@@ -7,7 +7,11 @@
 module ctrl (CLK, RST_F, OPCODE, MM, STAT, RF_WE, ALU_OP, WB_SEL, RD_SEL);
   
   /* TODO: Declare the ports listed above as inputs or outputs */
-  
+  // DONE
+  input  CLK, RST_F, STAT;
+  input  [3:0] OPCODE, MM; //OPCODE = Instr[31:28], MM = Instr[27:24]
+  output RF_WE, ALU_OP, WB_SEL, RD_SEL;
+
   // states
   parameter start0 = 0, start1 = 1, fetch = 2, decode = 3, execute = 4, mem = 5, writeback = 6;
    
@@ -23,10 +27,31 @@ module ctrl (CLK, RST_F, OPCODE, MM, STAT, RF_WE, ALU_OP, WB_SEL, RD_SEL);
   /* TODO: Write a clock process that progresses the fsm to the next state on the
        positive edge of the clock, OR resets the state to 'start0' on the negative edge
        of RST_F. Notice that the computer is reset when RST_F is low, not high. */
-
+  // DONE
+  always @ (posedge CLK or negedge RST_F)
+  begin
+    if (!RST_F) begin
+      present_state <= start0;
+    end
+    else begin
+      present_state <= next_state;
+    end
+  end
 
   /* TODO: Write a process that determines the next state of the fsm. */
-
+  // DONE
+  always @ (present_state)
+  begin    
+    case (present_state)
+      start0:    next_state <= start1;
+      start1:    next_state <= decode;
+      fetch:     next_state <= decode;     
+      decode:    next_state <= execute;
+      execute:   next_state <= mem;
+      mem:       next_state <= writeback;
+      writeback: next_state <= fetch;     
+    endcase
+  end
 
   // Halt on HLT instruction
   always @ (OPCODE)
@@ -40,7 +65,6 @@ module ctrl (CLK, RST_F, OPCODE, MM, STAT, RF_WE, ALU_OP, WB_SEL, RD_SEL);
     
   /* TODO: Generate outputs based on the FSM states and inputs. For Parts 2 and 3, you will
        add the new control signals here. */
-
   // fetch
     
   // decode
--- sisc.v
+++ sisc.v
@@ -3,6 +3,72 @@
 
 `timescale 1ns/100ps
 
-module sisc (CLK, RST_F, ir);
+module sisc (CLK, RST_F, IR);
+  input CLK, RST_F; 
+  input [31:0] IR;
+    
+  // Datapath and control signals
+  wire [3:0] mux4_result;
+	wire [31:0] wb_data;
+	wire [31:0] rsa;
+	wire [31:0] rsb;
+	wire rf_we;
+	wire imm_sel;
+	wire shf_ctl;
+	wire [1:0] log_ctl;
+	wire sub;
+	wire cc_en;
+	wire [3:0] cc;
+	wire [31:0] alu_result;
+	wire wb_sel;
+	wire [31:0] in_b;
+	wire [1:0] alu_op;
+	wire stat_out;
 
+  // Instantiate and connect all of the components
+  mux mux4   (.sel	       (rd_sel),
+			        .in_a        (IR[19:16]),
+			        .in_b        (IR[15:12]),
+			        .out         (mux4_result));
+
+	rf my_rf   (.read_rega   (IR[23:20]),
+		          .read_regb   (IR[19:16]),
+		          .write_reg   (mux4_result[3:0]),
+		          .write_data  (wb_data[31:0]),
+		          .rf_we       (rf_we),
+		          .rsa         (rsa),
+		          .rsb         (rsb));
+
+	alu	my_alu (.alu_op      (alu_op[1:0]),
+			        .rsa         (rsa[31:0]),
+			        .rsb         (rsb[31:0]),
+			        .imm	       (IR[15:0]),
+			        .cc 	       (cc),
+			        .alu_result  (alu_result));
+	
+	mux	mux32  (.sel	       (wb_sel),
+			        //.in_a      (alu_result[31:0]),
+			        //.in_b      (in_b),
+			        .in_a				 (1'b0),
+			        .in_b				 (alu_result[31:0])
+			        .out         (wb_data));
+
+  stat my_s  (.en          (stat_en)
+  						.in          (cc[3:0])
+  						.out         (stat_out));
+
+	fsm my_fsm (.CLK 	       (CLK),
+			        .RST_F       (RST_F),
+			        .OPCODE      (IR[31:28]),
+			        .MM          (IR[27:24]),
+			        .STAT        (stat_out),
+			        .RF_WE       (rf_we),
+			        .ALU_OP		   (alu_op),
+			        .WB_SEL      (wb_sel)
+			        .RD_SEL			 (rd_sel));
+                 
+  initial
+  begin
+    $monitor ($time,,," IR>: %h, R1=%h, R2=%h, R3=%h", IR, my_rf.ram_array[1], my_rf.ram_array[2], my_rf.ram_array[3]); 
+  end 
 endmodule
