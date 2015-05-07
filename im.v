9df3940f7ad38c4e90a4ac33074497023ea2d440
Matthew Thomas Fina <mfina@l-lnx131.engr.uiowa.edu>
2015-05-06 20:57:40 -0500

    all finished

--- im.v
+++ im.v
@@ -24,7 +24,7 @@ module im (read_addr, read_data);
 
   // load program
   initial begin : prog_load
-    $readmemh("sort_instr.data",ram_array);
+    $readmemh("mult_instr.data",ram_array);
   end
  
   // read process is sensitive to read address.
